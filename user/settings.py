733edbd3b40a23771642be25f0d4c82c37944c5d
Ankita Dey <ankitadeybeporoya@Ankitas-MacBook-Air.local>
2025-11-10 15:37:58 +0530

    updates

--- user/settings.py
+++ user/settings.py
@@ -175,6 +175,12 @@ import dj_database_url
 from dotenv import load_dotenv
 from django.core.exceptions import ImproperlyConfigured  
 load_dotenv()
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+import cloudinary_storage
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
